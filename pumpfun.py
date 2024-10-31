f3ee4142917ac0a9102386bf83b88d8e580729a7
devalmagno <devalmagno@gmail.com>
2024-10-31 09:37:18 -0300

    Swap ARB to PumpToken math added

--- pumpfun.py
+++ pumpfun.py
@@ -1,35 +1,31 @@
 def main():
-    x0 = 13051.7  # Initial reserve of Matic
+    x0 = 13051.7  # Initial reserve of ARB
     y0 = 1000000000.0  # Initial reserve of PumpToken
     fee = 0.0
     value_in_fees = 0.0  # Value in fees accumulated
-    max_x0 = 50000.0  # Target value for Matic reserve to reach
+    max_x0 = 50000.0  # Target value for ARB reserve to reach
 
     while True:
-        dx = float(input("Enter amount to buy in TokenGas: "))
+        dx = float(input("Enter amount to buy in ARB: "))
 
         # Calculate tentative dy and update value in fees
         dy = (dx * (1 - fee) * y0) / (x0 + dx * (1 - fee))
 
         if (y0 - dy < 206900000):
             dy = (y0 - 206900000)
-            dx = (boundingCurveY * (1 - fee) * x0) / (y0 - boundingCurveY)
-
-        else:
-
-
+            dx = (dy * (1 - fee) * x0) / (y0 - dy)
 
         value_in_fees += dx * fee
         y0 -= dy
         x0 += dx
 
         # Print results
-        print(f"\nTokenGas reserve: {x0}")
+        print(f"\nARB reserve: {x0}")
         print(f"PumpToken reserve: {y0}")
         print(f"Fee: {fee}")
-        print(f"Amount to buy in TokenGas: {dx}")
+        print(f"Amount to buy in ARB: {dx}")
         print(f"Amount to receive in PumpToken: {dy}")
-        print(f"Amount in fees accumulated: {value_in_fees}\n")
+        print(f"Amount in fees accumulated ($ARB): {value_in_fees}\n")
 
         if (y0 == 206900000):
             print("\nBounding Curve reached.")
